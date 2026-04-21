734fd78f81a4846038c4aaba2082e50f50bb035b
Toavandr <hei.toavina.2@gmail.com>
2026-04-21 16:49:45 +0300

    feat:add repository for member

--- src/main/java/com/example/agriculture/repository/MemberRepository.java
+++ src/main/java/com/example/agriculture/repository/MemberRepository.java
@@ -1,4 +1,20 @@
 package com.example.agriculture.repository;
 
+import com.example.agriculture.config.DataSource;
+import com.example.agriculture.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
 public class MemberRepository {
+    private DataSource dataSource;
+
+    public MemberRepository(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+
 }
