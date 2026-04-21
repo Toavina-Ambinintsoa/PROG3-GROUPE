8f0a8e4365c6f34ffa138b289aedd5554fc51c59
Andry-Tokiniaina <hei.tokiniaina.2@gmai.com>
2026-04-21 15:40:42 +0300

    feat: implement class MemberInformation

---
+++ src/main/java/com/example/agriculture/entity/MemberInformation.java
@@ -0,0 +1,18 @@
+package com.example.agriculture.entity;
+
+import com.example.agriculture.entity.Enum.Gender;
+import com.example.agriculture.entity.Enum.MemberOccupation;
+
+import java.time.LocalDate;
+
+public class MemberInformation {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Gender gender;
+    private String address;
+    private String profession;
+    private String phoneNumber;
+    private String email;
+    private MemberOccupation memberOccupation;
+}
