bd78316746f5568f0fcf65c9b55ded6f82f59c98
Andry-Tokiniaina <hei.tokiniaina.2@gmai.com>
2026-04-21 16:44:16 +0300

    fix: correction of collectivity repository attributes

--- src/main/java/com/example/agriculture/repository/CollectivityRepository.java
+++ src/main/java/com/example/agriculture/repository/CollectivityRepository.java
@@ -1,18 +1,32 @@
 package com.example.agriculture.repository;
 
+import com.example.agriculture.config.DataSource;
+import com.example.agriculture.entity.Collectivity;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class CollectivityRepository {
-    Connection connection;
+    DataSource ds;
 
-    public CollectivityRepository(Connection connection) {
-        this.connection = connection;
+    public CollectivityRepository(DataSource ds) {
+        this.ds = ds;
     }
 
-
+    List<Collectivity> CreateCollectivity(List<Collectivity> collectivity) {
+        String query = """
+                    INSERT INTO collectivity v
+                """;
+        try (Connection conn = ds.getConnection()){
+            PreparedStatement prstmt = conn.prepareStatement(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
