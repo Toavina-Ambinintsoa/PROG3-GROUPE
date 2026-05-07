d0d0681ede97f29638498a1aa08356becf3430e2
Toavandr <hei.toavina.2@gmail.com>
2026-05-07 13:08:20 +0300

    fix:remove package unused

--- src/main/java/com/example/agriculture/entity/Enum/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.agriculture.entity.Enum;
-
-public enum Gender {
-    MALE, FEMALE
-}
--- src/main/java/com/example/agriculture/entity/Enum/MemberOccupation.java
+++
@@ -1,5 +0,0 @@
-package com.example.agriculture.entity.Enum;
-
-public enum MemberOccupation {
-    PRESIDENT, VICE_PRESIDENT, SECRETARY, TRESORIER, CONFIRMED_MEMBER, JUNIOR_MEMBER
-}
